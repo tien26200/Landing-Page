a6a4881ee3fee73f014ea4cd3c4143e1d71744b1
tien26200 <doxuantien26200@gmail.com>
2022-02-25 15:47:40 +0700

    Landing-Page-Clock

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$currency = '$'; // đơn vị tiền tệ
+
+$db_username = 'root'; // user name
+
+$db_password = '';// password
+
+$db_name = '18-5_db'; // tên cơ sở dữ liệu
+
+$db_host = 'localhost';
+
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+
+?>
